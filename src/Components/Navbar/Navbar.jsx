3a821b2013e2d80468c778c30d664301c466581e
shamil <shamilmohd418@gmail.com>
2024-03-21 20:10:12 +0530

    sd

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,22 +7,23 @@ const Navbar = () => {
     <div className='NavbarMainDiv'>
       <div className="main">
         <div className="mainLeft">
-            <div className="navLinks">
-                <div className="links">HOME</div>
-                <div className="links">NEW ARRIVALS</div>
-                <div className="links">SHOP</div>
-                {/* <div className="links">COLLECTIONS</div> */}
-                <select name="" id="">
-                    <option value="">COLLECTIONS</option>
-                    <option value="">MEN</option>
-                    <option value="">WOMEN</option>
-                </select>
-                <div className="links">INSTAGRAM SHOP</div>
-                <div className="links">STORE</div>
-                <div className="links">INFLUENCER</div>
-            </div>
+          <div className="navLinks">
+            <Link to="/" className={`links ${location.pathname === '/' ? 'active' : ''}`}>HOME</Link>
+            <div className="links">NEW ARRIVALS</div>
+            <div className="links">SHOP</div>
+            <select name="" id="">
+              <option value="">COLLECTIONS</option>
+              <option value="">MEN</option>
+              <option value="">WOMEN</option>
+            </select>
+            <div className="links">INSTAGRAM SHOP</div>
+            <div className="links">STORE</div>
+            <div className="links">INFLUENCER</div>
+          </div>
         </div>
-        <div className="mainRight"><Link className='loginBtn'>Log In</Link></div>
+        <div className="mainRight">
+          <Link className='loginBtn'>Log In</Link>
+        </div>
       </div>
     </div>
   )
